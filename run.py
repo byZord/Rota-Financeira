cb92daffa21d53786a4a746ad798e5157813e9e4
byZord <arthurlucasbgspp@gmail.com>
2026-04-29 14:25:26 -0300

    Add files via upload

--- run.py
+++ run.py
@@ -1,45 +1,53 @@
+from flask import Flask, render_template, request
+import mysql.connector # Tradutor para implementar o BD
 
-from flask import Flask, render_template
-import mysql.connector #Tradutor para implementar o BD
-
-# Aqui estamos criando o aplicativo de fato. A variável app é o coração do site. O __name__ é uma configuração interna do python para ele saber onde achar as pastas do projeto
+# Aqui estamos criando o aplicativo de fato. A variável app é o coração do site.
 app = Flask(__name__) 
 
-#configuração da fechadura do banco de dados
+# Configuração da fechadura do banco de dados
 def conectar_banco():
     conexao = mysql.connector.connect(
-    host = "local_host",
-    user = "root",
-    password = "root",
-    database = "rota_financeira_db"
+        host = "localhost",
+        user = "root",
+        password = "root",
+        database = "rota_financeira_db"
     )
-    
     return conexao
 
-# Isso aqui pe a rota. É como se fosse o GPS da internet. O / significa a "pagina inicial" Você diz: "Quando alguém entrar na página inicial execute a função abaixo"
+# Rota da página inicial
 @app.route('/')
 def index():
-    
     return render_template('index.html')
 
-# Isso aqui é uma trava de segurança do Python. Ela garante que o servidor só vai ligar se você executar esse arquivo diretamente
-if __name__ == '__main__':
-    # Essa função faz com que toda vez que o código for salvo o servidor reinicie sozinho e aplique as mudanças, além de mostrar os erros detalhados na tela caso algo dê errado
-    app.run(debug=True) 
-
-#rota para testar o banco de dados
-@app.route('/Testar-banco')
+# Rota para testar o banco de dados
+@app.route('/testar-banco')
 def testar_banco():
     try:
-        #Tenta abrir a conexão e fechar logo em seguida
-        conexao = conectar_banco
+        # Tenta abrir a conexão e fechar logo em seguida
+        conexao = conectar_banco()
         conexao.close()
-        return "<h1>Conexão com banco de dados feita com sucesso🚀</h1>"
-    
-    #Uma mensagem caso a conexão falhe (ele indica o motivo)
+        return "<h1>Conexão com banco de dados feita com sucesso</h1>"
     except Exception as e:
         return f"<h1>Erro ao tentar conectar banco: {e}</h1>"
-    
+
+# Rota para receber os dados do usuario
+@app.route('/adicionar-transacao', methods=['POST'])
+def adicionar_transacao():
+    # Primeiro capturar os dados usando os "names" que o front-end colocou lá no HTML
+    descricao = request.form.get('descricao')
+    valor = request.form.get('valor')  
+    tipo = request.form.get('tipo')
+
+    # Imprimir no terminal do VS code para auditar as transações
+    print("-----------------------------------------------")
+    print(f" DADOS RECEBIDOS DO FRONT-END:")
+    print(f"Descrição: {descricao}")
+    print(f"Valor: R$ {valor}")
+    print(f"Tipo: {tipo}")
+    print("-----------------------------------------------")
+
+    return f"<h1>Dados de '{descricao}' recebidos com sucesso pelo Motor Python! </h1>"
+
+# Trava de segurança - DEVE SER SEMPRE A ÚLTIMA COISA DO ARQUIVO!
 if __name__ == '__main__':
     app.run(debug=True)
-
--- setup_banco_sq.sql
+++ setup_banco_sq.sql
@@ -30,3 +30,14 @@ CREATE TABLE IF NOT EXISTS transacoes (
 
 SELECT * FROM transacoes;
 
+-- Passo 5: Inserir um Usuário de Teste
+-- Como o ID é automático, não precisamos informá-lo.
+INSERT INTO usuarios (nome, email, senha)
+VALUES ('Administrador Teste', 'admin@teste.com', 'senha123');
+-- Passo 6: Inserir Transações de Teste para o usuário 1
+-- Vamos colocar 3 transações diferentes
+INSERT INTO transacoes (usuario_id, descricao, valor, tipo, data_transacao)
+VALUES
+(1, 'Salário Mensal', 3500.00, 'receita','2026-04-05'),
+(1,'Supermercado Extra', 450.50, 'despesa', '2026-04-10'),
+(1,'Conta de Luz', 120.00, 'despesa', '2026-04-15');
