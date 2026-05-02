f089036160bc5ac634dc43f3b6e74cb74bb9171e
byZord <arthurlucasbgspp@gmail.com>
2026-05-02 16:16:47 -0300

    Salvando as correcoes locais no motor do Python

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, render_template
 import mysql.connector #Tradutor para implementar o BD
+from datetime import date
 
 # Aqui estamos criando o aplicativo de fato. A variável app é o coração do site. O __name__ é uma configuração interna do python para ele saber onde achar as pastas do projeto
 app = Flask(__name__) 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 #configuração da fechadura do banco de dados
 def conectar_banco():
     conexao = mysql.connector.connect(
-    host = "local_host",
+    host = "localhost",
     user = "root",
     password = "root",
     database = "rota_financeira_db"
@@ -22,17 +23,12 @@ def index():
     
     return render_template('index.html')
 
-# Isso aqui é uma trava de segurança do Python. Ela garante que o servidor só vai ligar se você executar esse arquivo diretamente
-if __name__ == '__main__':
-    # Essa função faz com que toda vez que o código for salvo o servidor reinicie sozinho e aplique as mudanças, além de mostrar os erros detalhados na tela caso algo dê errado
-    app.run(debug=True) 
-
 #rota para testar o banco de dados
-@app.route('/Testar-banco')
+@app.route('/testar-banco')
 def testar_banco():
     try:
         #Tenta abrir a conexão e fechar logo em seguida
-        conexao = conectar_banco
+        conexao = conectar_banco()
         conexao.close()
         return "<h1>Conexão com banco de dados feita com sucesso🚀</h1>"
     
@@ -40,6 +36,7 @@ def testar_banco():
     except Exception as e:
         return f"<h1>Erro ao tentar conectar banco: {e}</h1>"
     
+# Isso aqui é uma trava de segurança do Python. Ela garante que o servidor só vai ligar se você executar esse arquivo diretamente
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    # Essa função faz com que toda vez que o código for salvo o servidor reinicie sozinho e aplique as mudanças, além de mostrar os erros detalhados na tela caso algo dê errado
+    app.run(debug=True) 
